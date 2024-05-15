b3367eda7c440dd061f9c95e61678cfef458d0b9
Sheillahatukwatse <sheillahatukwatse30@gmail.com>
2024-05-15 00:38:12 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$con=mysql-connect("localhost","root","SHEILLAH");
+if(!$con){
+    die "failed to connect";
+}
+else{
+    echo "connection sucessful";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <title>connect your database</title>
+</head>
+<body>
+    
+</body>
+</html>
